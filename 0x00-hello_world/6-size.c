a27ea58a291999887434597d4fd7fb2b7331e1a1
Safahadidy <safahadidy@gmail.com>
2023-03-16 19:59:21 -0700

    v

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * */
+
+int main(void)
+{
+	print("Size of a char: %zu byte(s)\n", sizeof(char));
+	print("Size of a char: %zu byte(s)\n", sizeof(char));
+	print("Size of a long int);
+}
