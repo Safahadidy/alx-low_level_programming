b22ce8f10de5313d5873eef6a6b0c99fef149ac8
Safahadidy <safahadidy@gmail.com>
2023-03-17 19:26:03 -0700

    c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit number
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	} 
+	printf("\n");
+	 return (0);
+}
