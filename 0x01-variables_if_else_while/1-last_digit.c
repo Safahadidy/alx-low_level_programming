8de25a935d26326bc53a6015ccc3a0713f5e1aa5
Safahadidy <safahadidy@gmail.com>
2023-03-20 19:06:56 -0700

    c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,15 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
- * main - prints a text accroding number
+ * main - enrty point
  *
- * Description: print the value of n status: greater than, is zero, less than 6.
+ * Description: print the value of n status:
+ *		greater than, is zero and is not less than 6.
  *
- * Return: Always succes */
+ * Return: 0 (success)
+*/
 
 int main(void)
 {
@@ -18,11 +18,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	digit = n % 10;
-if (digit > 5)
-	printf("Last digit of %i is %i and is less greater than 5\n", n, digit);
-else if (digit == 0)
-	printf("Last digit of %i is %i and is 0\n", n, digit");
-else if (digit < 6 && digit != 0)
-	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
+
+	if (digit > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
+	else if (digit == 0)
+		printf("Last digit of %i is %i and is 0\n", n, digit);
+	else if (digit < 6 && digit != 0)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
 	return (0);
 }
