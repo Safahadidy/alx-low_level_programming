4db8cb69349bb8df9f27061799b007e017428ef3
Safahadidy <safahadidy@gmail.com>
2023-03-20 19:08:59 -0700

    b

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,19 +3,21 @@
 /**
  * main - Entry point
  *
- * Description: prints all alphapatic letters.
+ * Description: print all aplhabet letters
  *
  * Return: Always 0 (Success)
 */
-int main(void)
-{	char ch = 'a';
-	while (ch <= 'z')
-			{
-		       	putchar(ch);
-			ch++;
-			}
-			putchar('\n');
-return (0); 
-}
 
-			
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}			
