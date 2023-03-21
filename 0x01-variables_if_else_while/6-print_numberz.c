07cf9fde0c9928b4f3b6a390a434e07fba66777d
Safahadidy <safahadidy@gmail.com>
2023-03-20 19:28:26 -0700

    d

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,21 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: print 0-9 using putchar while using int variable
+ * Description: print 0-9 using putchar
+ *              while using int variable
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-        int digit = 0;
-        while (digit <= 9)
-        {
-	   /*correct digit to ASCII representation*/
-	putchar(digit + '0');
-digit++;
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		/*convert digit to ASCII representation*/
+		putchar(digit + '0');
+		digit++;
 	}
 	putchar('\n');
+
 	return (0);
 }
