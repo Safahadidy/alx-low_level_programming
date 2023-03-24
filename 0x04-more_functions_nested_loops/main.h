679ab79e89eecfc20f62cbf7ee2f181f4f89c675
Safahadidy <safahadidy@gmail.com>
2023-03-23 20:39:01 -0700

    c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,10 @@ int _isupper(int c);
 int _isdigit(int c);
 void print_numbers(void);
 int mul(int a, int b);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+
 
 #endif
