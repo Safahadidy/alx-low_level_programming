c33db04d7e958f180a44e312581130507f4212d0
Safahadidy <safahadidy@gmail.com>
2023-04-05 03:24:39 -0700

    c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,15 +2,18 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: The string to be measured.
+ * @n: The length of a string
  * Return: the length of a string.
  */
 int _strlen_recursion(char *s);
 {
-int len = 0;
-if (*s == '\0')
-return (len);
-else
-len ++i;
-len +=  _strlen_recursion(s + 1);
-return (len);
+int _strlen_recursion(char *s)
+{
+int n = 0;
+if (*s)
+{
+n++;
+n += _strlen_recursion(s + 1);
+}
+return (n);		
 }
