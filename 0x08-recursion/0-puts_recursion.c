4565bcd080779c1d17f42ee040333dd9d15199c4
Safahadidy <safahadidy@gmail.com>
2023-04-05 02:08:52 -0700

    rec

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * _puts_recursion();
  * @s: input
@@ -9,7 +9,7 @@ void _puts_recursion(char *s)
 if (*s)
 {
 _putchar(*s);
-_put_recursion(s+1);
+_puts_recursion(s+1);
 }
 else
 _putchar('\n');
