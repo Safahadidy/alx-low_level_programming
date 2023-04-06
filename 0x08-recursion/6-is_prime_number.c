e7e40d3f7a4f73d45223ad82585831462c7f77d4
Safahadidy <safahadidy@gmail.com>
2023-04-05 19:27:25 -0700

    c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,39 +1,30 @@
 #include "main.h"
 /**
- * is_prime_number - says if an integer is a prime number or not
- * @n: number to evaluate
- *
- * Return: 1 if n is a prime number, 0 if not
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked.
+ * @div: The divisor.
+ * Return: 1 if n is a div number, 0 if not
+ */
+int is_divisible(int num, int div)
+{
+if (num % div == 0)
+return (0);
+if (div == num / 2)
+return (1);
+return (is_divisible(num, div + 1));
+}
+/**
+ *is_prime_number - says if an integer is a prime number or not
+ *@n: number to evaluate
+ *Return: 1 if n is a prime number, 0 if not
  */
 int is_prime_number(int n)
 {
+int div = 2;
 if (n <= 1)
 return (0);
-return (actual_prime(n, n - 1));
+if (n >= 2 && n <= 3)
+return (1);
+return (is_divisible(n, div));
 }
 
-/**
- * primeno - calculates if a number is prime recursively
- * @num: number to evaluate
- * @i: iterator
- *
- * Return: 1 if n is prime, 0 if not
- */
-int primeno(int num, int i)
-{
-if (i == 1)
-{
-return 1;
-}
-else
-{
-if (num % i == 0)
-{
-return 0;
-}
-else
-{
-return primeno(num, i - 1);
-} 
-}
-}
