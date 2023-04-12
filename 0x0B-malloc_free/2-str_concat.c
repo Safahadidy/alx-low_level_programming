c257834bd319f28342eed62c059beb89d7a0472e
Safahadidy <safahadidy@gmail.com>
2023-04-12 16:25:18 -0700

    c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,31 +8,36 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-char *concat;
-int i, con;
+char *s;
+int i, j;
+
 if (s1 == NULL)
+
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-i = con = 0;
+i = j = 0;
 while (s1[i] != '\0')
 i++;
-while (s2[con] != '\0')
-con++;
-concat = malloc(sizeof(char) * (i + ci + 1));
-if (concat == NULL)
+
+while (s2[j] != '\0')
+j++;
+
+s = malloc(sizeof(char) * (i + j + 1));
+if (s == NULL)
 return (NULL);
-i = con = 0;
+
+i = j = 0;
 while (s1[i] != '\0')
 {
-concat[i] = s1[i];
+s[i] = s1[i];
 i++;
 }
-while (s2[con] != '\0')
+while (s2[j] != '\0')
 {
-concat[i] = s2[con];
-i++, con++;
+s[i] = s2[j];
+i++, j++;
 }
-conct[i] = '\0';
-return (concat);
+s[i] = '\0';
+return (s);
 }
