dfb4ca57c9a1259207a49175942f209f20b1eb5e
Safahadidy <safahadidy@gmail.com>
2023-04-12 16:50:28 -0700

    c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,12 +9,15 @@
   */
 char *create_array(unsigned int size, char c)
 {
-char *array;
+char *str;
 unsigned int i;
-array = malloc(sizeof(char) * size);
-if (size == 0 || array == '\0')
+if (size == 0)
+return (NULL);
+str = malloc(sizeof(char) * size);
+
+if (str == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
-array[i] = c;
-return (array);
+str[i] = c;
+return (str);
 }
