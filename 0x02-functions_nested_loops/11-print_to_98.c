7c8ae9792b637f502193820b4543b13323a9c945
Safa hadidy <125626986+Safahadidy@users.noreply.github.com>
2023-05-15 03:53:42 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,30 +7,36 @@
 * followed by a new line
 * @n: print from this number
 */
-void print_to_98(int n)
+void print_times_table(int n)
 {
-int i, j;
-
-
-if (n <= 98)
-{
-for (i = n; i <= 98; i++)
-{
-if (i != 98)
-printf("%d, ", i);
-else if (i == 98)
-printf("%d\n", i);
+    int i, j, prod;
+    if ((n>=0)&&(n<=15))
+    {
+        for(i=0;i<=n;i++)
+        {
+            for(j=0; j<=n;j++)
+            {
+                prod=i*j;
+                if((prod>=0)&&(prod<10))
+                _putchar(prod + '0');
+                else if ((prod>=10)&&(prod<100))
+                {
+                    _putchar(prod/10 + '0');
+                    _putchar(prod%10 + '0');
+                }    
+                else
+                {
+                    _putchar(prod/100 + '0');
+                    _putchar((prod/10)%10 + '0');
+                    _putchar(prod%10 + '0');
+                }
+            
+                 _putchar(',');
+                  _putchar(' ');
+            }
+             _putchar('\n');
+        }
+        
+    }
+    
 }
-}
-else if (n >= 98)
-{
-for (j = n; j >= 98; j--)
-{
-if (j != 98)
-printf("%d, ", j);
-else if (j == 98)
-printf("%d\n", j);
-}
-}
-}
-
