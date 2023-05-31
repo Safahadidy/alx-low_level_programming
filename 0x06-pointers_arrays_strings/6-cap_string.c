106b90b04c9a9339bb2ae42d78f90b2eca51d69d
Safa hadidy <125626986+Safahadidy@users.noreply.github.com>
2023-05-31 07:08:37 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,28 +6,34 @@
  *
  * Return: A pointer to the changed string.
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-int index = 0;
+int i, j;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-while (str[index])
-{
-while (!(str[index] >= 'a' && str[index] <= 'z'))
-index++;
-
-if (str[index - 1] == ' ' ||
-str[index - 1] == '\t' ||
-str[index - 1] == '\n' ||
-str[index - 1] == ',' ||
-str[index - 1] == ';' ||
-str[index - 1] == '.' ||
-str[index - 1] == '!' ||
-str[index - 1] == '?' ||
-str[index - 1] == '"' ||
-str[index - 1] == '(' ||
-str[index - 1] == ')' ||
-str[index - 1] == '{' ||
-str[index - 1] == '}' ||
+	
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (i == 0)
+			{
+				s[i] = s[i]-32;
+			}
+			else
+			{
+				for (j = 0; j < 13; j++)
+				{
+					if (a[j] == s[i-1])
+					
+						s[i] = *(s + i) - 32;
+					
+				}
+			}
+		}
+	i++;
+	}
+	return (s);
 index == 0)
 str[index] -= 32;
 
