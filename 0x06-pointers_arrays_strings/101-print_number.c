c7c2254946bf0ba01322f5e82bde85b9badc3404
Safa hadidy <125626986+Safahadidy@users.noreply.github.com>
2023-06-03 06:34:24 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,19 +8,16 @@
 
 void print_number(int n)
 {
-unsigned int n1;
-
-n1 = n;
-
-if (n < 0)
-{
-_putchar('-');
-n1 = -n;
+	if (n<0)
+    {	
+        _putchar('-');
+	    n*=-1;
+    }
+    if (n>=0&&n<=9)
+    _putchar(n+'0');
+    else if (n>9)
+ {    
+     print_number(n/10);
+     _putchar(n%10+'0');
 }
-
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
 }
