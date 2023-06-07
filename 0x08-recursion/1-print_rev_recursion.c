013ee83046a2d1c520a0d2fff279bb58a3aa1f31
Safahadidy <safahadidy@gmai.com>
2023-06-07 00:20:05 -0700

    c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* Description: _print_rev_recursion-Print String using recursion
+* @s: Pointer To String
+* Return: void
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+return;
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
