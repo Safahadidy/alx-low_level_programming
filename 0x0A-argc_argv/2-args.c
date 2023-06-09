7cad84a0746b643d6816206ddece41088f8cd70c
Safahadidy <safahadidy@gmai.com>
2023-06-09 14:01:58 -0700

    c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* description: main - Program That Prints It's Name
+* @argc: The Args Count
+* i : string in array
+* @argv: The Array Or Args
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
