9c5aa6383cbb0a38e7a888d89fbd4514e268d89c
Safahadidy <safahadidy@gmail.com>
2023-07-01 09:19:21 -0700

    c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,9 @@
  * print_before_main - Prints a string before the
  *        main function is executed.
  */
+void before_main(void) __attribute__((constructor));
 
-void before_main() __attribute__((constructor));
-void before_main()
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
